979e51461f7cfee086a47f8cf88861844fc0fa5c
liuajianzhi <liuajianzhi_1992@hotmail.com>
2018-03-28 12:22:31 +0800

    123

---
+++ mp3InfoTest/mp3InfoTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace mp3InfoTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+
+            //TagLib.Mpeg.AudioFile  file= new TagLib.Mpeg.AudioFile(@"F:\音乐\伊藤由奈 - trust you.mp3");
+            //TagLib.Tag tag = file.Tag;
+
+            //Console.WriteLine(tag.FirstArtist);
+            //Console.WriteLine(tag.Album);
+            //Console.WriteLine(tag.Pictures);
+
+            //List<string> Info = new List<string>();
+
+            //TagLib.File f = TagLib.File.Create(@"F:\音乐\孙燕姿 - 180度.mp3");
+
+            //Info.Add($"专辑：{f.Tag.Album}"
+            //);
+            //Info.Add($"歌手：{f.Tag.FirstAlbumArtist}");
+            //Info.Add($"曲名：{f.Tag.Title}");
+            //foreach(string str in Info)
+            //{
+            //    Console.WriteLine(str);
+            //}
+
+
+            //var ss= GetCover(@"F:\音乐\孙燕姿 - 180度.mp3");
+            //if(ss!=null)
+            //{
+            //    Image image=Image.FromStream(ss);
+            //    FileStream fs = new FileStream($"MusicImgs/{f.Tag.Title}.jpg", FileMode.Create);
+            //    image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //}
+
+            TagLibSharpSub tagSub = new TagLibSharpSub();
+            tagSub.init(@"F:\音乐\Avril Lavigne - When You're Gone.mp3");
+            List<string> info = tagSub.GetInfo();
+            string path = tagSub.GetImagePath();
+            foreach(string s in info)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine(path);
+
+            Console.ReadLine();
+        }
+
+        public static MemoryStream GetCover(string path)
+        {
+            TagLib.File f = TagLib.File.Create(path);
+            if (f.Tag.Pictures != null && f.Tag.Pictures.Length != 0)
+            {
+                var bin = (byte[])(f.Tag.Pictures[0].Data.Data);
+                return new MemoryStream(bin);
+            }
+            else
+                return null;
+
+        }
+    }
+}
---
+++ mp3InfoTest/mp3InfoTest/TagLibSharpSub.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagLib;
+using TagLib.Id3v2;
+using System.Drawing;
+using System.IO;
+
+namespace mp3InfoTest
+{
+    class TagLibSharpSub
+    {
+        public TagLib.File mFile;
+        public string Mpath { set; get; }
+        public List<string> Info;
+
+        public TagLib.Tag GetTag(string path)
+        {
+            return mFile.Tag;
+        }
+
+        public List<string> GetInfo()
+        {
+            Info.Add($"标题：{mFile.Tag.Title}");
+            Info.Add($"歌手：{mFile.Tag.FirstAlbumArtist}");
+            Info.Add($"专辑：{mFile.Tag.Album}");
+            return Info;
+        }
+        public string GetImagePath()
+        {
+            string CoverPath;
+           // FileStream imgFs = new FileStream($"MusicImgs/{mFile.Tag.Title}.jpg", FileMode.Open);
+            CoverPath = $"MusicImgs/{mFile.Tag.Title}.jpg";
+            if (System.IO.File.Exists(CoverPath))
+            {
+                CoverPath=$"MusicImgs/{mFile.Tag.Title}.jpg";
+            }
+            else
+            {
+                var ss = GetCover();
+                if (ss != null)
+                {
+                    Image image = Image.FromStream(ss);
+                    FileStream fs = new FileStream($"MusicImgs/{mFile.Tag.Title}.jpg", FileMode.Create);
+                    image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    CoverPath = $"MusicImgs/{mFile.Tag.Title}.jpg";
+
+                }
+                else
+                {
+                    CoverPath = $"";
+                }
+
+            }
+
+            return CoverPath;
+        }
+
+        public  MemoryStream GetCover()
+        {
+            TagLib.File f = mFile;
+            if (f.Tag.Pictures != null && f.Tag.Pictures.Length != 0)
+            {
+                var bin = (byte[])(f.Tag.Pictures[0].Data.Data);
+                return new MemoryStream(bin);
+            }
+            else
+                return null;
+
+        }
+        public void init(string path)
+        {
+            Mpath = path;
+            mFile = TagLib.File.Create(Mpath);
+            Info = new List<string>();
+        }
+    }
+}
